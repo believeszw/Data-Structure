f4a89f0004d87eb17b2c8246c7f19d8ab7930599
believeszw <37264337+believeszw@users.noreply.github.com>
2018-12-26 16:05:45 +0800

    Add files via upload

---
+++ Data_structure/cmake-build-debug/CMakeFiles/Data_structure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data_structure.dir/main.cpp.obj"
+  "Data_structure.pdb"
+  "Data_structure.exe"
+  "Data_structure.exe.manifest"
+  "libData_structure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Data_structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
